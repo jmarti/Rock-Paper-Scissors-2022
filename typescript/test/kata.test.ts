0cd43b1fe0f3d6153ff559448e15737784303d1c
Chaparron <chaparroncoder@gmail.com>
2023-09-21 23:15:06 +0200

    parameterized testing

--- typescript/test/kata.test.ts
+++ typescript/test/kata.test.ts
@@ -1,44 +1,28 @@
-import { expect, describe, it } from "vitest";
-import { RockPaperScissorGame } from "../src/kata";
+import { expect, describe, it } from "vitest"
+import { RockPaperScissorGame } from "../src/kata"
 
 describe("Rock paper scissors game", () => {
+
   it("constructor does not throw", () => {
-    expect(() => new RockPaperScissorGame("", "")).not.toThrowError();
-  });
-
-  it("can initialize a game", () => {
-    const game = new RockPaperScissorGame("", "");
-    expect(game).toBeInstanceOf(RockPaperScissorGame);
-  });
-
-  describe("I want the rock to beat scissors", () => {
-    it("Then I should win", () => {
-      const player = "rock";
-      const opponent = "scissors";
-      const game = new RockPaperScissorGame(player, opponent);
-      expect(game.winner()).toBe(player);
-    });
-
-    it("Then the opponent win", () => {
-      const player = "scissors";
-      const opponent = "rock";
-      const game = new RockPaperScissorGame(player, opponent);
-      expect(game.winner()).toBe(opponent);
-    });
-  });
-
-  it("I want the paper to beat rock", () => {
-    const player = "paper";
-    const opponent = "rock";
-    const game = new RockPaperScissorGame(player, opponent);
-    expect(game.winner()).toBe(player);
-  });
-
-  it("Given I have chosen rock. When the opponent chooses paper. Then the opponent should win", () => {
-    const player = 'rock'
-    const opponent = 'paper'
-    const game = new RockPaperScissorGame(player, opponent)
-    expect(game.winner()).toBe(opponent)
+    expect(() => new RockPaperScissorGame("", "")).not.toThrowError()
   })
 
-});
+  it("can initialize a game", () => {
+    const game = new RockPaperScissorGame("", "")
+    expect(game).toBeInstanceOf(RockPaperScissorGame)
+  })
+
+  const testCases = [
+    { player: "rock", opponent: "scissors", expectedWinner: "rock" },
+    { player: "scissors", opponent: "rock", expectedWinner: "rock" },
+    { player: "paper", opponent: "rock", expectedWinner: "paper" },
+    { player: "rock", opponent: "paper", expectedWinner: "paper" },
+  ]
+
+  testCases.forEach(({ player, opponent, expectedWinner }) => {
+    it(`if player shows ${player} and opponent shows ${opponent}, winner will be ${expectedWinner}`, () => {
+      const game = new RockPaperScissorGame(player, opponent)
+      expect(game.winner()).toBe(expectedWinner)
+    })
+  })
+})
