e45ed5ec787e9050f33255bbd6c4ff28b032d5ba
Xiphereal <lozanojalex@gmail.com>
2023-09-21 19:12:32 +0200

    style: formateo

--- typescript/test/kata.test.ts
+++ typescript/test/kata.test.ts
@@ -1,13 +1,16 @@
 import { expect, describe, it } from "vitest";
 import { RockPaperScissorGame } from "../src/kata";
+
 describe("Rock paper scissors game", () => {
   it("constructor does not throw", () => {
     expect(()=> new RockPaperScissorGame("","")).not.toThrowError();
   })
+
   it("can initialize a game", () => {
     const game = new RockPaperScissorGame("","")
     expect(game).toBeInstanceOf(RockPaperScissorGame);
   })
+  
   it("I want the rock to beat scissors", () => {
     const player = 'rock'
     const opponent = 'scissors'
